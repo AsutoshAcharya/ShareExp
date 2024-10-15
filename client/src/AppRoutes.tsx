9d8c69b7f1444cdc86f809ac30200bb44043fb4b
Asutosh Acharya <asutosh246@gmail.com>
2024-10-15 22:25:04 +0530

    changes

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,16 +1,16 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-const Login = React.lazy(()=>import('./screens/LogIn/Login'))
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+const Login = React.lazy(() => import("./screens/LogIn"));
 const AppRoutes = () => {
   return (
     <Router>
-       <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
       </Suspense>
     </Router>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
