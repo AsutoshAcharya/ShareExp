db4d634f95d0e3763ed8680d43e74fb89acdc879
Asutosh Acharya <asutosh246@gmail.com>
2024-10-21 00:28:58 +0530

    minor changes in basic  Input for error case

--- client/src/components/BasicInput.tsx
+++ client/src/components/BasicInput.tsx
@@ -18,11 +18,14 @@ const BasicInput: FC<Props> = ({ label, helperText, ...rest }) => {
       )}
       <input
         id={label}
-        className="mt-1 block w-full px-4 py-2 bg-gray-50 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:placeholder-gray-400 dark:text-white"
+        className={
+          `mt-1 block w-full px-4 py-2 bg-gray-50 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:placeholder-gray-400 dark:text-white` +
+          (helperText ? " focus:border-red-400" : "")
+        }
         {...rest}
       />
       {helperText && (
-        <p className="block text-sm font-medium text-red-500 dark:text-red-500">
+        <p className="block text-sm font-medium text-red-400 dark:text-red-400">
           {helperText}
         </p>
       )}
