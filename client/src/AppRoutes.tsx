1d271b7c85d6eb15b791d298aba00350407ba49f
Asutosh Acharya <asutosh246@gmail.com>
2024-10-20 15:56:22 +0530

    added react-toastify

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Fragment, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -9,21 +9,34 @@ import { BlurryLoader } from "./components";
 import appRoutes from "./AppRoutes/appRoutes";
 import { groupBy } from "./helpers";
 import GetRoute from "./AppRoutes/GetRoute";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const allRoutes = groupBy(appRoutes, "kind");
 const AppRoutes = () => {
   return (
-    <Router>
-      <Suspense fallback={<BlurryLoader />}>
-        <Routes>
-          {/* {allRoutes["private"].map(GetRoute)} */}
-          {allRoutes["public"].map(GetRoute)}
-          {/* {allRoutes["independent"].map(GetRoute)} */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-          
-        </Routes>
-      </Suspense>
-    </Router>
+    <Fragment>
+      <ToastContainer
+        pauseOnFocusLoss={false}
+        position="bottom-left"
+        pauseOnHover={false}
+        style={{
+          width: "fit-content",
+          fontSize: "13px",
+        }}
+      />
+
+      <Router>
+        <Suspense fallback={<BlurryLoader />}>
+          <Routes>
+            {/* {allRoutes["private"].map(GetRoute)} */}
+            {allRoutes["public"].map(GetRoute)}
+            {/* {allRoutes["independent"].map(GetRoute)} */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </Fragment>
   );
 };
 
