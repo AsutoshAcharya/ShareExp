50658eaf8ebee49eecda9b99877f244dbffb739a
Bidhu1024 <bidhu1024av@gmail.com>
2024-11-07 19:34:00 +0530

    Header of profile page

--- client/src/screens/Profile/index.tsx
+++ client/src/screens/Profile/index.tsx
@@ -1,5 +1,35 @@
 const Profile = () => {
-  return <div>Profile</div>;
-};
+  return (
+    <div className="h-100% width-100% p-6">
+      <div className="w-100% flex justify-between">
+        <div>
+          <button
+            type="button"
+            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            back
+          </button>
+        </div>
+        <div>
+        <button
+          type="button"
+          className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >
+          cancel
+        </button>
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          save changes
+        </button>
+        </div>
+     
+      </div>
+      <div>
 
+      </div>
+    </div>
+  );
+};
 export default Profile;
